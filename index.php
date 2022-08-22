12f753a797ba4a5aad2bfda02b692a436690c198
Sagar Bisht <itssagar11@gmail.com>
2022-08-22 22:04:14 +0530

    21 aug

--- index.php
+++ index.php
@@ -1,63 +1,131 @@
+
+
+<!-- Copyright (c) 2021 Sagar Bisht  -->
+
+<!-- --- ----- ----- --- -- - - - -- - -- - -->
+<!doctype>
 <html>
-    <?php require_once("config/head.php")?>
+  <head>
+     <meta data-rh="true" name="theme-color" content="#000000" />
+     <link
+      href="https://fonts.googleapis.com/css2?family=Rubik:wght@362&display=swap"
+      rel="stylesheet"
+    />
+    <meta
+      name="viewport"
+      content="width=device-width,minimum-scale=1,initial-scale=1"
+    />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/login.css">
+    <title> Swatchta-SignIn</title>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="sweetalert2.min.js"></script>
-<link rel="stylesheet" href="sweetalert2.min.css">
-    <body>
-        <div class="container" style="width:350px; margin-top:10%;">
-            <form>
-    <!-- mobile input -->
-    <div class="form-outline mb-4">
-    <label class="form-label" for="form2Example1">Mobile No</label>
-        <input type="email" id="mobile_no" class="form-control" />
-        
-    </div>
+    <style>
 
-    <!-- Password input -->
-    <div class="form-outline mb-4">
-    <label class="form-label" for="form2Example2">Password</label>
-        <input type="password" id="password" class="form-control" />
+      body{
+        margin:10px;
+        display:flex;
+         font-family: "Rubik", sans-serif;
+        justify-content:space-around;
+        align-items:center;
+        flex-wrap:wrap;
+        
+      }
+      .form-container{
+        margin:20px;
+        
+        width:400px;
+      }
+      .branding{
+        width:350px;
+      }
+      input{
+        margin:15px 1px 15px 1px;
+        width:95%;
+        height:30px;
+        padding:2px 2px;
+        border:none;
+        border-bottom:1px solid #878787;
+        
+        
+       
+      }
+      input[type=button]{
+        background-color:#00A14D;
+        border:1px solid #00A14D;
+        color:#ffffff;
+        padding:10px;
+        width:80%;
+        border-radius:20px;
+        fot-size:16px;
+        font-width:normal;
+      }
+      input:focus{
+        outline:none;
+        width:96%;
+      }
+      hr{
+        width:10%;
+        height:3px;
+        background-color:#FCA435;
+        border:none;
+      }
+      .quotes{
+        color:#FCA435;
+        font-size:2rem;
+      }
+      .otp_box{
+        display:none;
+      }
     
-    </div>
-
-    <!-- 2 column grid layout for inline styling -->
-    <div class="row mb-4">
-        <div class="col d-flex justify-content-center">
-        <!-- Checkbox -->
-        <div class="form-check">
-           <br>
-        </div>
-        </div>
-
-        <div class="col">
-        <!-- Simple link -->
-      
-        </div>
-    </div>
-
-    <!-- Submit button -->
-    <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-    <!-- Register buttons -->
-    <div class="text-center">
-        <p>Dont have account? <a href="register.php">Register</a></p>
+    </style>
+  </head>
+  <body>
+    <div class="form-container">
+         <center>
+      <h1 style="color:#FCA435">
+        Sign in 
+      </h1>
+  <p style="color:#FCA435">
+        Enter the Mobile Number and Password associated with your account.
+      </p>
+            <!-- Login -->
+            <input type="tel" class="textbox login_box" id="no" name="txt_user" placeholder="Mobile Number" /><br>
         
+            <input type="Password" class="textbox login_box" id="pass"  name="txt_pwd" placeholder="Password"/><br>
+        
+            <input type="button" value="LOG IN" name="but_submit" id="but_submit"  class="login_box btn-block"/>
+           <br> <span>or</span><br>
+           <a href="registor.php" style="color:FCA435;">Create an account</a>
+            <small id="message"></small>
+        
+</center> 
     </div>
-    </form>
-        </div>
-   
-    </body>
-
+    <div class="branding">
+      <center>
+      <hr>
+      <img src="images/logo1.png" width="250px">
+      <h3 class="quotes">
+      
+      </h3>
+  
+        </center>
+    </div>
+  </body>
+ 
+</html>
 
     <script>
         $(".btn-block").click(function(){
-            let no= $("#mobile_no").val();
-            let pass=$("#password").val();
+            $("#message").html("<br><span style='color:#15c18b;'>Please wait..</span>");
+            let no= $("#no").val();
+            let pass=$("#pass").val();
             $.ajax({
                 url:"login.php",
                 type:"POST",
                 data:{no:no,pass:pass},
                 success:function(res){
+                    $("#message").html("");
                     if(res==0){
                         Swal.fire({
   position: 'top-end',
